8eeada4c17fbd5391a435c8e70b5477182367b2e
Richards <decagon@Decagons-MacBook-Air.local>
2022-10-06 20:52:28 +0100

    Added new test case to ApplicantServiceImplTest

--- src/test/java/com/shop/models/ApplicantServiceImplTest.java
+++ src/test/java/com/shop/models/ApplicantServiceImplTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicantServiceImplTest {
     @Test
-    void apply() {
+    void applicationSuccessful() {
         Address tomAddress = new Address("Rabbi Street", "Port Harcourt", "Rivers");
         Applicant applicant3 = new Applicant("AP095", "Thompson", "Leke",
                 234814098123L, "kolade.lola@gmail.com", tomAddress, Qualification.HND, 3);
@@ -17,5 +17,17 @@ class ApplicantServiceImplTest {
         String result = applicantService.apply(applicant3);
 
         assertEquals("You have been hired as a Cashier.", result);
+    }
+
+    @Test
+    void applicationUnsuccessful() {
+        Address tomAddress = new Address("Rabbi Street", "Port Harcourt", "Rivers");
+        Applicant applicant3 = new Applicant("AP095", "Thompson", "Leke",
+                234814098123L, "kolade.lola@gmail.com", tomAddress, Qualification.MSC, 3);
+
+        ApplicantService applicantService = new ApplicantServiceImpl();
+        String result = applicantService.apply(applicant3);
+
+        assertNotEquals("You have been hired as a Cashier.", result);
     }
 }
